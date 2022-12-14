30728921f630371337b8c5b1589b08f5194318c9
Enrique Alberto Navia <anavia_n@outlook.com>
2022-12-14 12:37:36 -0600

    Add last example of not like

--- EjemplosLike.sql
+++ EjemplosLike.sql
@@ -1,5 +1,7 @@
-SELECT * FROM AP.dbo.Vendors
-WHERE VendorState LIKE 'N[A-J]' 
+--SELECT * FROM AP.dbo.Vendors WHERE VendorState LIKE 'N[A-J]' 
 
-SELECT * FROM AP.dbo.Vendors
-WHERE VendorState LIKE 'N[^K-Y]' 
+--SELECT * FROM AP.dbo.Vendors WHERE VendorState LIKE 'N[^K-Y]' 
+
+--Traemos los zipcodes que empiezan con 0's 
+SELECT VendorZipCode, * FROM AP.dbo.Vendors
+WHERE VendorZipCode NOT LIKE '[1-9]%'
