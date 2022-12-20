a03c2c05d6f1e532e1df94155ebda6db2553a4a7
Enrique Alberto Navia <anavia_n@outlook.com>
2022-12-20 17:41:29 -0600

    Update EjemplosLike.sql

--- EjemplosLike.sql
+++ EjemplosLike.sql
@@ -3,5 +3,10 @@
 --SELECT * FROM AP.dbo.Vendors WHERE VendorState LIKE 'N[^K-Y]' 
 
 --Traemos los zipcodes que empiezan con 0's 
-SELECT VendorZipCode, * FROM AP.dbo.Vendors
-WHERE VendorZipCode NOT LIKE '[1-9]%'
+--SELECT VendorZipCode, * FROM AP.dbo.Vendors WHERE VendorZipCode NOT LIKE '[1-9]%'
+
+--SELECT * FROM Examples.dbo.NullSample WHERE InvoiceTotal <> 0
+
+--SELECT * FROM Examples.dbo.NullSample WHERE InvoiceTotal IS NOT NULL
+
+SELECT * FROM Examples.dbo.NullSample WHERE InvoiceTotal IS NULL
