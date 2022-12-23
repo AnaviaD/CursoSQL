783425c4eb09b52b8f540560eb90fcc16a634503
Enrique Alberto Navia <anavia_n@outlook.com>
2022-12-22 21:19:55 -0600

    Add the second example

--- 58 GROUP-BY_EXCERC.sql
+++ 58 GROUP-BY_EXCERC.sql
@@ -10,3 +10,12 @@ SELECT VendorState, VendorCity, COUNT(*) AS InvoiceQty,
         ON I.VendorID = V.VendorID
 GROUP BY VendorState, VendorCity
 ORDER BY VendorState, VendorCity        
+
+--The same summary query with a HAVING clause that limits the results
+SELECT VendorState, VendorCity, COUNT(*) AS InvoiceQty,
+    AVG(InvoiceTotal) AS InvoiceAVG
+        FROM AP.dbo.Invoices AS I JOIN AP.dbo.Vendors AS V
+        ON I.VendorID = V.VendorID
+    GROUP BY VendorState, VendorCity
+    HAVING COUNT(*) >= 2
+    ORDER BY VendorState, VendorCity
