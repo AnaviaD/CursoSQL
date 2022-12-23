bb910411b19e69eec13cd35635f6ecf80a3dd7b9
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-23 11:13:09 -0600

    Add Description to having where

--- 59 Comparing-HAVING-WHERE.sql
+++ 59 Comparing-HAVING-WHERE.sql
@@ -24,3 +24,13 @@ obtienen aquellos que sean mayores a 500
 2.- En el segundo ejemplo primero se Obtienen lo que son mayor a 500
 y despues se agrupan los resultados */
 
+/*
+A HAVING clause is like a WHERE clause, but applies only to groups as a whole (that is, to the rows in the result set representing groups), 
+whereas the WHERE clause applies to individual rows. A query can contain both a WHERE clause and a HAVING clause. In that case:
+
+The WHERE clause is applied first to the individual rows in the tables or table-valued objects in the Diagram pane. 
+Only the rows that meet the conditions in the WHERE clause are grouped.
+
+The HAVING clause is then applied to the rows in the result set. Only the groups that meet the HAVING conditions appear in the query output. 
+You can apply a HAVING clause only to columns that also appear in the GROUP BY clause or in an aggregate function.
+*/
