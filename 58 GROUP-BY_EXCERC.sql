bbc477ad23f151c1f6419c0b3fa7099a120f902d
Enrique Alberto Navia <anavia_n@outlook.com>
2022-12-22 21:14:18 -0600

    Add second example of group by

---
+++ 58 GROUP-BY_EXCERC.sql
@@ -0,0 +1,12 @@
+--A summary query that counts the number of invoices by vendor
+SELECT VendorID, COUNT(*) AS InvoiceQty
+FROM AP.dbo.Invoices
+GROUP BY VendorID
+
+--A summary query that groups by two columns
+SELECT VendorState, VendorCity, COUNT(*) AS InvoiceQty,
+    AVG(InvoiceTotal) AS InvoiceAVG
+        FROM AP.dbo.Invoices AS I JOIN AP.dbo.Vendors AS V
+        ON I.VendorID = V.VendorID
+GROUP BY VendorState, VendorCity
+ORDER BY VendorState, VendorCity        
