c6fcc1e3817926723200f32921b3649a632a811b
Enrique Alberto Navia <anavia_n@outlook.com>
2022-12-28 11:47:59 -0600

    Add some observations of how is it used

--- 63 How-to-use-OVER.sql
+++ 63 How-to-use-OVER.sql
@@ -8,7 +8,12 @@ SELECT InvoiceNumber, InvoiceDate, InvoiceTotal,
 	COUNT(InvoiceTotal) OVER (PARTITION BY InvoiceDate) AS DateCount,
 	AVG(InvoiceTotal) OVER (PARTITION BY InvoiceDate) AS DateAvg
 		FROM AP.dbo.Invoices
--- Creo que el over se aplica solo con la condicion de que se pone en el PARTITION BY 
+-- Creo que el over se aplica solo con la condicion de que se pone en el PARTITION BY
+-- Los datos son asi como vienen y el PARTITION BY sirve para cumplir justo esa condicion sin importar los rows que vengan
+-- Cuando se aplique la SUM() OVER (PARTITION BY InvoiceDate) como se toman los valores como vienen, NO SE MUESTRA EL PROCESO DE LA SUMA
+-- Simplemente muestra el total de la operacion, no como el ejemplo de abajo
+-- Esto es como en el caso de que aparecen 3 en la columna DATECOUNT pero
+-- En las columnas donde aparece 1 como se puede ver no hay ningun cambio en DATESUM O DATE AVG	
 
 
 /*		2
@@ -19,3 +24,4 @@ SELECT InvoiceNumber, InvoiceDate, InvoiceTotal,
 	COUNT(InvoiceTotal) OVER (ORDER BY InvoiceDate) AS CountT,
 	AVG(InvoiceTotal) OVER (ORDER BY InvoiceDate) AS MovingAvg
 		FROM AP.dbo.Invoices
+		ORDER BY InvoiceDate
