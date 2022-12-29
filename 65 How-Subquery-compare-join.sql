8ba6b1185028d6b9d8d22ce69e24a6b2f401bd47
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-29 11:31:05 -0600

    Subquery vs inner join

--- 65 How-Subquery-compare-join.sql
+++ 65 How-Subquery-compare-join.sql
@@ -16,3 +16,24 @@ SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
 				FROM AP.dbo.Vendors
 				WHERE VendorState = 'CA')
 		ORDER BY InvoiceDate DESC
+
+
+-- Demostramos que trae una lista de donde tomar los valores
+-- Es el mismo query que arriba pero en lugar de el subquery vamos a poner la lista como tal
+SELECT InvoiceNumber, InvoiceDate, InvoiceTotal
+	FROM AP.dbo.Invoices
+		WHERE VendorID IN
+			(4, 6, 7)
+		ORDER BY InvoiceDate DESC
+
+/*
+Ventajas de los joins
+1.- Los resultados de un join pueden traer columnas de ambas tablas
+2.- Un join tiende a ser mas intuitivos cuando hay una relacion entre 2 tablas 
+3.- Un query con un join usualmente se ejecuta mas rapido que un subquery
+
+Ventajas de Subqueries
+1.- Un subquery puede pasar un valor agregado "aggregate value to the outer query"
+2.- Un subquery tiende a ser mas intuitivo cuando se usa "ad hoc relationship between two tables"
+3.- Long, complex queries can sometimes be easier to code using subqueries
+*/		
