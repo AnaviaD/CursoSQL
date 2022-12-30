877a23ce5a1c815b6d0dfb03a75fce1ffa32312f
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-30 09:37:22 -0600

    Add furst two excercices from HR

---
+++ HR-3.sql
@@ -0,0 +1,11 @@
+/*
+Query all columns (attributes) for every row in the CITY table.
+The CITY table is described as follows:
+*/
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT * FROM CITY;
---
+++ HR-4.sql
@@ -0,0 +1,12 @@
+/*
+Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows:
+*/
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+SELECT * FROM CITY WHERE ID = '1661';
