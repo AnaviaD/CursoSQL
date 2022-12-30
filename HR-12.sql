a7b118745c83e44b32c1d46763a1772ebec4ba2e
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-30 11:47:50 -0600

    AD the triangle CASE Excersice from SQL

--- HR-12.sql
+++ HR-12.sql
@@ -1,4 +1,4 @@
--- Weather Observation Station 7
+-- Weather Observation Station 8
 /*
 Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both 
 their first and last characters. Your result cannot contain duplicates.
